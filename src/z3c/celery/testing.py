8802f46a44e42315753057a8332986fdc7254e3e
Michael Howitz <mh@gocept.com>
2016-12-16 14:35:02 +0100

    Reduce duplicate code by using `login_principal()` in `.celery`, too.

--- src/z3c/celery/testing.py
+++ src/z3c/celery/testing.py
@@ -1,14 +1,3 @@
-import zope.publisher.browser
-import zope.security.management
-
-
-def login_principal(principal):
-    """Start an interaction with `principal`."""
-    request = zope.publisher.browser.TestRequest()
-    request.setPrincipal(principal)
-    zope.security.management.newInteraction(request)
-
-
 ZOPE_CONF_TEMPLATE = """
 site-definition {ftesting_path}
 
