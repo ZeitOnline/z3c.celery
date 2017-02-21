911f5799a4c45a2479b3b7aa5faf15dfe5678cef
Michael Howitz <mh@gocept.com>
2017-02-21 14:26:20 +0100

    Preparing release 0.1

--- CHANGES.rst
+++ CHANGES.rst
@@ -2,7 +2,7 @@
 Change log for z3c.celery
 =========================
 
-0.1 (unreleased)
+0.1 (2017-02-21)
 ================
 
 - Initial release. Extract from zeit.cms.
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='z3c.celery',
-    version='0.1.dev0',
+    version='0.1',
 
     install_requires=[
         'celery >= 4.0.2',
